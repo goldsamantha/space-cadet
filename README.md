e2a791c52996a47e2134041b4ea246fd009b2720
Samantha <samantha.goldstein31@gmail.com>
2015-10-05 17:29:13 -0400

    stable

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # space-cadet
+
+TODO:
+
+1. Make generic wallpaper
+2. Add Space images from NASA's apod
+3. Push to appstore
+4. Make look good.
+5. ??!!?
+6. Prophet/Profit
---
+++ wallpaper.js
@@ -0,0 +1,2 @@
+var new_img = document.getElementsByClassName('apod')[0];
+document.body.style.background = "url('http://apod.nasa.gov/apod/image/1510/tle_dierickD8D_1002ozoneC.jpg') center fixed";
