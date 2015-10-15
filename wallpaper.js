68ca8679cf116f9c7c03d33bd4cf544fc57ed17e
Weston Vial <wes@wesvial.com>
2015-10-15 14:42:05 -0400

    Correctly load APOD image as background

--- wallpaper.js
+++ wallpaper.js
@@ -1,24 +1,17 @@
-//var new_img = document.getElementsByClassName('apod')[0];
-//document.body.style.background = "url('http://apod.nasa.gov/apod/image/1510/tle_dierickD8D_1002ozoneC.jpg') center fixed";
-var img2;
+var new_img = document.getElementsByClassName('apod')[0];
+document.body.style.background = "url('http://apod.nasa.gov/apod/image/1510/tle_dierickD8D_1002ozoneC.jpg') center fixed";
+
+
 function setImage() {
 	$.get('http://apod.nasa.gov/apod/astropix.html', function(data) {
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(data, 'text/html');
-	var x = doc.querySelector('img')
-	img2 = $(x).attr('src');
-	console.log(img2);
-});
-
-
-
-
+		var parser = new DOMParser();
+		var doc = parser.parseFromString(data, 'text/html');
+		var x = doc.querySelector('img')
+		img2 = $(x).attr('src');
+		console.log(img2);
+		var new_img = document.getElementsByClassName('apod')[0];
+		document.body.style.background = "url('http://apod.nasa.gov/apod/" + img2 + "') center fixed";
+	});
 }
 
-$.get('http://apod.nasa.gov/apod/astropix.html', function(data) {
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(data, 'text/html');
-	var x = doc.querySelector('img')
-	img2 = $(x).attr('src');
-	console.log(img2);
-});
+$(document).ready(setImage());
