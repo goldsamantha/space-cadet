7adf7a0536d626cc19a3e390f397469f07b139c1
Weston Vial <wes@wesvial.com>
2015-10-15 20:28:46 -0400

    CSS wip

--- wallpaper.js
+++ wallpaper.js
@@ -4,9 +4,19 @@ function setBackgroundImage() {
         var doc = parser.parseFromString(data, 'text/html');
         var img_link = doc.querySelectorAll('a')[1];
         var img_url = "http://apod.nasa.gov/apod/" + $(img_link).attr('href');
-        var new_img = document.getElementsByClassName('apod')[0];
         document.body.style.background = "url('" + img_url + "') no-repeat center center fixed";
     });
 }
 
+function updateClock() {
+    var date = new Date();
+    var date_today = date.toLocaleString(undefined, {month: "long", day: "numeric", year: "numeric"});
+    var time_now = date.toLocaleTimeString(undefined, {hour: "numeric", minute: "numeric", second: "numeric"});
+    document.getElementById('clock').innerHTML = time_now + "<br>" + date_today;
+    setTimeout(updateClock, 100);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateClock();
+});
 $(document).ready(setBackgroundImage());
