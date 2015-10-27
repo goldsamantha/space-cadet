5f9a4760b36b106eb7e8b4ab23220b51bf34640e
Weston Vial <wes@wesvial.com>
2015-10-27 14:24:19 -0400

    No longer adds background to every page

--- wallpaper.js
+++ wallpaper.js
@@ -2,11 +2,13 @@ function setBackgroundImage() {
     $.get('http://apod.nasa.gov/apod/astropix.html', function(data) {
         var parser = new DOMParser();
         var doc = parser.parseFromString(data, 'text/html');
+        console.log(doc);
         var img_link = doc.querySelectorAll('a')[1];
         var img_url = "http://apod.nasa.gov/apod/" + $(img_link).attr('href');
         document.body.style.background = "url('" + img_url + "') no-repeat center center fixed";
     });
 }
+
 
 function updateClock() {
     var date = new Date();
@@ -15,8 +17,8 @@ function updateClock() {
     var period = time_now.slice(time_now.length-2, time_now.length);
     var time_now = time_now.slice(0, time_now.length-3);
 
-    document.getElementById('time').innerHTML   = time_now;
-    document.getElementById('date').innerHTML   = date_today;
+    document.getElementById('time').innerHTML = time_now;
+    document.getElementById('date').innerHTML = date_today;
     document.getElementById('period').innerHTML = period;
     setTimeout(updateClock, 100);
 }
