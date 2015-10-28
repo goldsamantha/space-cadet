1ddb69f7269bff8875fe6862f71cc288e65ddfcf
Weston Vial <wes@wesvial.com>
2015-10-28 13:40:35 -0400

    Weather widget working and repositioned. Loads Blood Moon photo if the APOD image is unavailable. Some styling changes.

--- wallpaper.js
+++ wallpaper.js
@@ -7,36 +7,55 @@ function setBackgroundImage() {
         var imgLink = doc.querySelectorAll('a')[1];
         // Check if link is img
         var urlExtension = $(imgLink).attr('href').slice(-4).toLowerCase();
+        var sourceHtml = '<a href="http://apod.nasa.gov">NASA Astronomical Picture of the Day</a>';
         if (urlExtension === '.jpg') {
             var imgUrl = "http://apod.nasa.gov/apod/" + $(imgLink).attr('href');
             document.body.style.background = "url('" + imgUrl + "') no-repeat center center fixed";
-            document.getElementById('source').innerHTML = '<a href="http://apod.nasa.gov">NASA Astronomical Picture of the Day</a>';
+            document.getElementById('source').innerHTML = sourceHtml;
         } else {
             document.body.style.background = "url('bloodmoon.jpg') no-repeat center center fixed";
+            document.getElementById('source').innerHTML = '<a href="http://apod.nasa.gov/apod/ap151003.html">A Blue Blood Moon</a><br>' + sourceHtml;
         }
     });
 }
 
-/*function getWeather() {
-    $.ajax({
-        url: "http://api.wunderground.com/api/d7718ae963a47176/geolookup/conditions/q/IA/Cedar_Rapids.json",
-        dataType: "jsonp",
-        success: function(parsed_json) {
-            var location = parsed_json['location']['city'];
-            var temp_f = parsed_json['current_observation']['temp_f'];
-//            alert("current temp in " + location + " is: " + temp_f);
-            document.getElementById('weather').innerHTML = 'current temp in ' + location + ' is ' + temp_f;
-        }
-    });
-} */
+function getGeolocation() {
+    if ('geolocation' in navigator) {
+        console.log('Geoloc available');
+        console.log(navigator['geolocation']);
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getWeather(position.coords.latitude, position.coords.longitude);
+        });
+    } else {
+        console.log('Geoloc unavailable');
+        document.getElementById('weather').innerHTML = 'Location information is unavailable, sorry!';
+    }
+}
 
-function getWeather() {
+function getWeather(lat, long) {
+    if (lat === undefined || long === undefined) {
+        return;
+    }
     // Get weather data
-    var uri = "http://api.wunderground.com/api/d7718ae963a47176/geolookup/conditions/q/NY/Brooklyn.json";
+    var uri = "http://api.wunderground.com/api/" + wundergroundKey + "/geolookup/conditions/q/" + lat + "," + long + ".json";
+    console.log('Latitude, longitude', lat, long);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status == 200) {
-            console.log(xhr.responseText);
+            var j = JSON.parse(xhr.responseText);
+            console.log(j);
+            var weather = j['current_observation']['weather'];
+            var temp = j['current_observation']['temp_f'];
+            var location = j['location']['city'];
+            if (j['location']['country'] === 'US') {
+                location += ", " + j['location']['state'];
+            } else {
+                location += ", " + j['location']['country'];
+            }
+            var weatherUrl = j['current_observation']['ob_url'];
+            var html = '<a href="' + weatherUrl + '">';
+            html += weather + " | " + temp + "&#176F<br>" + location + '</a>';
+            document.getElementById('weather').innerHTML = html;
         }
     };
     xhr.open('GET', uri);
@@ -58,8 +77,8 @@ function updateClock() {
 
 document.addEventListener('DOMContentLoaded', function() {
     updateClock();
+    getGeolocation();
 //    getWeather();
-    weather2();
 });
 
 $(document).ready(setBackgroundImage());
